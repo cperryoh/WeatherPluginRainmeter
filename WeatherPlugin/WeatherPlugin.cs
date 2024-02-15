d3fcb7076423e7adcdb84510e52a2484846d21b2
cole perry <cperryoh@gmail.com>
2024-02-14 20:19:53 -0500

    fix with weather upadte

--- WeatherPlugin/WeatherPlugin.cs
+++ WeatherPlugin/WeatherPlugin.cs
@@ -14,23 +14,22 @@ using Rainmeter;
 
 namespace WeatherPlugin
 {
-    public class Main
+    public class Current
     {
         public double temp { get; set; }
         public double temp_max { get; set; }
         public double temp_min { get; set; }
         public double humidity { get; set; }
+        public Weather[] weather { get; set; }
+    }
+    public class Root {
+        public Current current { get; set; }
     }
     public class Weather
     {
-        public String main { get; set; }
+        public String Current { get; set; }
         public String description { get; set; }
         public String icon { get; set; }
-    }
-    public class MainJson
-    {
-        public Weather[] weather { get; set; }
-        public Main main { get; set; }
     }
     class Measure
     {
@@ -40,7 +39,7 @@ namespace WeatherPlugin
         }
 
         public IntPtr buffer = IntPtr.Zero;
-        public MainJson mainJson;
+        public Root mainJson;
         public API api;
         public String apiKey;
         public double lon;
@@ -73,39 +72,34 @@ namespace WeatherPlugin
             {
 
                 //get json from api
-                String json = new WebClient().DownloadString("https://" + $"api.openweathermap.org/data/2.5/weather?lat={measure.lat}&lon={measure.lon}&appid={measure.apiKey}");
-                MainJson weather = (new JavaScriptSerializer()).Deserialize<MainJson>(json);
+                String json = new WebClient().DownloadString("https://"+$"api.openweathermap.org/data/2.5/onecall?lat={measure.lat}&lon={measure.lon}&appid={measure.apiKey}");
+                Root weather = (new JavaScriptSerializer()).Deserialize<Root>(json);
                 measure.mainJson = weather;
 
                 //determain which object we are going to return
                 if (measure.type.Equals("temp") || measure.type.Equals(""))
                 {
-                    measure.data = convert(weather.main.temp, measure.units).ToString();
+                    measure.data = convert(weather.current.temp, measure.units).ToString();
                     api.Log(API.LogType.Debug, "Skin getting temp");
                 }
                 if (measure.type.Equals("temp_min"))
                 {
-                    measure.data = convert(weather.main.temp_min, measure.units).ToString();
+                    measure.data = convert(weather.current.temp_min, measure.units).ToString();
                     api.Log(API.LogType.Debug, "Skin getting temp_min");
                 }
                 if (measure.type.Equals("temp_max"))
                 {
-                    measure.data = convert(weather.main.temp_max, measure.units).ToString();
+                    measure.data = convert(weather.current.temp_max, measure.units).ToString();
                     api.Log(API.LogType.Debug, "Skin getting temp_man");
                 }
                 if (measure.type.Equals("humidity"))
                 {
-                    measure.data = weather.main.humidity.ToString();
+                    measure.data = weather.current.humidity.ToString();
                     api.Log(API.LogType.Debug, "Skin getting humidity");
-                }
-                if (measure.type.Equals("condition"))
-                {
-                    measure.data = weather.weather[0].main;
-                    api.Log(API.LogType.Debug, "Skin getting condition");
                 }
                 if (measure.type.Equals("description"))
                 {
-                    measure.data = weather.weather[0].description;
+                    measure.data = weather.current.weather[0].description;
                     api.Log(API.LogType.Debug, "Skin getting description");
                 }
             }
@@ -147,38 +141,34 @@ namespace WeatherPlugin
 
             try
             {
-                String json = new WebClient().DownloadString("https://" + $"api.openweathermap.org/data/2.5/weather?lat={measure.lat}&lon={measure.lon}&appid={measure.apiKey}");
-                MainJson weather = (new JavaScriptSerializer()).Deserialize<MainJson>(json);
+                String json = new WebClient().DownloadString("https://" + $"api.openweathermap.org/data/2.5/onecall?lat={measure.lat}&lon={measure.lon}&appid={measure.apiKey}");
+                Root weather = (new JavaScriptSerializer()).Deserialize<Root>(json);
                 measure.mainJson = weather;
                 if (measure.type.Equals("temp") || measure.type.Equals(""))
                 {
-                    measure.data = convert(weather.main.temp, measure.units).ToString();
-                    return convert(weather.main.temp, measure.units);
+                    measure.data = convert(weather.current.temp, measure.units).ToString();
+                    return convert(weather.current.temp, measure.units);
                 }
                 if (measure.type.Equals("temp_min"))
                 {
-                    measure.data = convert(weather.main.temp_min, measure.units).ToString();
-                    return convert(weather.main.temp_min, measure.units);
+                    measure.data = convert(weather.current.temp_min, measure.units).ToString();
+                    return convert(weather.current.temp_min, measure.units);
                 }
                 if (measure.type.Equals("temp_max"))
                 {
-                    measure.data = convert(weather.main.temp_max, measure.units).ToString();
-                    return convert(weather.main.temp_max, measure.units);
+                    measure.data = convert(weather.current.temp_max, measure.units).ToString();
+                    return convert(weather.current.temp_max, measure.units);
                 }
                 if (measure.type.Equals("humidity"))
                 {
-                    measure.data = weather.main.humidity.ToString();
-                    return weather.main.humidity;
-                }
-                if (measure.type.Equals("condition"))
-                {
-                    measure.data = weather.weather[0].main;
+                    measure.data = weather.current.humidity.ToString();
+                    return weather.current.humidity;
                 }
                 if (measure.type.Equals("description"))
                 {
-                    measure.data = weather.weather[0].description;
+                    measure.data = weather.current.weather[0].description;
                 }
-                return convert(weather.main.temp, measure.units);
+                return convert(weather.current.temp, measure.units);
             }
             catch (Exception e)
             {
@@ -237,35 +227,31 @@ namespace WeatherPlugin
             measure.api.Log(API.LogType.Debug, "Running get value");
             if (argv[0].Equals("temp") || argv[0].Equals(""))
             {
-                outVal = convert(measure.mainJson.main.temp, measure.units).ToString();
+                outVal = convert(measure.mainJson.current.temp, measure.units).ToString();
             }
             if (argv[0].Equals("temp_min"))
             {
-                outVal = convert(measure.mainJson.main.temp_min, measure.units).ToString();
+                outVal = convert(measure.mainJson.current.temp_min, measure.units).ToString();
             }
             if (argv[0].Equals("temp_max"))
             {
-                outVal = convert(measure.mainJson.main.temp_max, measure.units).ToString();
+                outVal = convert(measure.mainJson.current.temp_max, measure.units).ToString();
             }
             if (argv[0].Equals("humidity"))
             {
-                outVal = measure.mainJson.main.humidity.ToString();
-            }
-            if (argv[0].Equals("condition"))
-            {
-                outVal = measure.mainJson.weather[0].main;
+                outVal = measure.mainJson.current.humidity.ToString();
             }
             if (argv[0].Equals("description"))
             {
-                outVal = measure.mainJson.weather[0].description;
+                outVal = measure.mainJson.current.weather[0].description;
             }
             if (argv[0].Equals("iconUrl"))
             {
-                outVal = "http://" + $"openweathermap.org/img/wn/{measure.mainJson.weather[0].icon}@2x.png";
+                outVal = "http://" + $"openweathermap.org/img/wn/{measure.mainJson.current.weather[0].icon}@2x.png";
             }
             if (argv[0].Equals("icon"))
             {
-                outVal = measure.mainJson.weather[0].icon;
+                outVal = measure.mainJson.current.weather[0].icon;
             }
             measure.api.Log(API.LogType.Debug, $"Returning {outVal}");
             return Marshal.StringToHGlobalUni(outVal);
